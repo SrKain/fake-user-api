785cec551f0fea8983ea182809c628ec3950d0f2
SrKain <134458062+SrKain@users.noreply.github.com>
2024-05-23 13:25:54 -0300

    versão inicial da API, adicionando README.md ao projeto

---
+++ README.md
@@ -0,0 +1,5 @@
+# API criada usando express.js
+
+Utilizei variaveis fixas para facilitar o desenvolvimento, entretanto, ela não permite que duas instâncias compartilhem informações, para isso, seria necessário o uso de um banco de dados.
+
+Como até o momento eu não tinha trabalhado com o desenvolvimento direto de uma API, precisei recorrer a um tutorial simplificado usando express.
--- app.js
+++ app.js
@@ -19,9 +19,9 @@ let user = [
 ];
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // ou '*' para permitir todas as origens
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header("Access-Control-Allow-Origin", "*"); // ou '*' para permitir todas as origens
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
